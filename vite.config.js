b5d8855e128f96116c3d86fbcdd9016f2efe113a
Anton Merkulau <anton.merkulov@icloud.com>
2025-10-25 21:57:17 +0200

    Refactored Vite configuration to simplify the start script in package.json and added server and preview settings for dynamic host and port management.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const allowedHosts = [process.env.HOST].filter(Boolean);
+const port = process.env.PORT || 5173;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    allowedHosts
+  },
+  preview: {
+    host: allowedHosts[0],
+    port,
+    allowedHosts
+  }
 })
